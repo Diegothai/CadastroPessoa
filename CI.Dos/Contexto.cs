90e21a2a35c0850078e17a49913172d49d347cf4
Diegothai <diego.mellosll@gmail.com>
2018-09-20 01:05:09 -0300

    Meu primeiro commit

---
+++ CI.Dos/Contexto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI.Dos
+{
+    class Contexto : IDisposable
+    {
+        private readonly SqlConnection minhaConexao;
+
+        public Contexto()
+        {
+            minhaConexao = new SqlConnection(ConfigurationManager.ConnectionStrings["CadastroDePessoasConfig"].ConnectionString);
+            minhaConexao.Open();
+        }
+
+        public void ExecutaComando(string strQuery)
+        {
+            var cmdComando = new SqlCommand
+            {
+                //new SqlCommand(strQueryDelete, minhaConexao);
+                CommandText = strQuery,
+                CommandType = CommandType.Text,
+                Connection = minhaConexao
+            };
+            cmdComando.ExecuteNonQuery();
+        }
+
+        public SqlDataReader ExecuteComandoComRetorno(string strQuery)
+        {
+            var cmdComando = new SqlCommand(strQuery, minhaConexao);
+            return cmdComando.ExecuteReader();
+        }
+
+        public void Dispose()
+        {
+            if (minhaConexao.State == ConnectionState.Open)
+                {
+                    minhaConexao.Close();
+                }
+        }
+    }
+}
---
+++ CI.Dos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI.Dos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var contexto = new Contexto();
+
+            Console.Write("Digite seu Nome ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite a data de nascimento ");
+            string data = Console.ReadLine();
+
+            Console.Write("Digite o número do RG ");
+            string rg = Console.ReadLine();
+
+            Console.Write("Digite o cnpj de sua Empresa ");
+            string cnpj = Console.ReadLine();
+
+            Console.Write("Digite o Endereço ");
+            string endereco = Console.ReadLine();
+
+            string strQueryInsert = string.Format("INSERT INTO Pessoa (Nome, DataNascimento, Num_rg, Num_cnpj, Endereco) VALUES ('{0}','{1}','{2}','{3}','{4}')",
+                nome, data, rg, cnpj, endereco);
+
+            contexto.ExecutaComando(strQueryInsert);
+
+            string strQuerySelect = "SELECT * FROM PESSOA";
+
+            SqlDataReader dados = contexto.ExecuteComandoComRetorno(strQuerySelect);
+
+
+            while (dados.Read())
+            {
+                Console.WriteLine("Id: {0}, Nome: {1}, DataNascimento {2}, Num_rg {3}, Num_cnpj {4}, Endereco {5}", dados["PessoaId"], dados["Nome"], dados["DataNascimento"], dados["Num_rg"], dados["Num_cnpj"], dados["Endereco"]);
+            }
+
+
+        }
+    }
+}
